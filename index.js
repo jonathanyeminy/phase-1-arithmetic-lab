514d80c2d65739807b4ee3c8f878cc0df40a8004
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-03-15 10:22:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2
+
+let random = Number.parseInt(10*(Math.random()))
+
+const num3=5
+const num4=24
+let mod=num4 % num3
+let max= Math.max(1,5,9,14,18,20)
